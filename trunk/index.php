aa1c20977d714b35dc910611a16da9dea8f27573
shareaza <shareaza@592ba7bc-5a53-0410-a536-97c50bdddb5b>
2008-08-02 01:04:33 +0000

    Record IP addresses of URLs, gave priority to good caches when testing, removed function, fixed some stuff

--- trunk/index.php
+++ trunk/index.php
@@ -14,54 +14,12 @@
 	// You should have received a copy of the GNU General Public License
 	// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-	// Tests GWebCache for valid output
-	function test_url($url) {
-		$error = TRUE;
-		if(preg_match(URL_REGEX, $url, $matches)) {
-			$domain = $matches['domain'];
-			$ip = gethostbyname($domain);
-			$ip = $ip != $domain ? $ip : ''; // Make sure IP is blank if domain is returned by function
-			if($ip) {
-				$port = isset($matches['port']) && $matches['port'] ? $matches['port'] : 80;
-				ini_set('user_agent', 'Cachechu');
-				$socket = @fsockopen($domain, $port, $errno, $errstr, 5);
-				if($socket) {
-					$file = isset($matches['file']) ? $matches['file'] : '/'; // No need to URL encode
-					$out = "GET $file?get=1&net=gnutella2&client=TEST&version=Cachechu HTTP/1.0\r\n";
-					$out .= "Host: $domain\r\n";
-					$out .= "Connection: Close\r\n\r\n";
-					$response = '';
-					if(@fwrite($socket, $out) !== FALSE) {
-						stream_set_timeout($socket, 5);
-						$response = stream_get_contents($socket);
-					}
-					fclose($socket);
-					$pos = strpos($response, "\r\n\r\n");
-					if($pos !== FALSE) {
-						$contents = trim(substr($response, $pos + 4));
-						if($contents) {
-							$error = FALSE;
-						}
-						$lines = explode("\n", $contents);
-						foreach($lines as $line) {
-							if(!preg_match(OUTPUT_REGEX, $line)) {
-								$error = TRUE; // Contains invalid output
-								break;
-							}
-						}
-					}
-				}
-			}
-		}
-		return !$error; // Return TRUE if it works
-	}
-
 	define('HOST_PATH', 'store/hosts');
 	define('URL_PATH', 'store/urls');
-	define('HOST_LIMIT', 50); // Only store 50 hosts
+	define('HOST_LIMIT', 30); // Only store 30 hosts
 	define('MAX_HOST_AGE', 9000); // 2.5 hours
 	define('MAX_URL_AGE', 86400); // 24 hours
-	define('IP_REGEX', '/\\A(?:(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):\\d+)\\z/');
+	define('IP_REGEX', '/\\A((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)):(\\d+)\\z/');
 	define('URL_REGEX', '/\\Ahttp:\/\/(?P<domain>[-A-Z0-9.]+)(?::(?P<port>[0-9]+))?(?P<file>\/[-A-Z0-9+&@#\/%=~_|!:,.;]*)?\\z/i');
 	define('OUTPUT_REGEX', '%\\A(?:(?:H\\|(?:[0-9]{1,3}\\.){3}[0-9]{1,3}.*)|(?:U\\|http://.+)|(?:[A-GI-TV-Z]\\|.*))\\z%i');
 
@@ -73,7 +31,7 @@
 	$update = isset($_GET['update']) ? $_GET['update'] : '';
 	$url    = isset($_GET['url']) ? trim($_GET['url']) : '';
 
-	if(count($_GET) > 0) {
+	if(!empty($_GET)) {
 		header('Content-Type: text/plain');
 		if(strtolower($net) != 'gnutella2') {
 			if(!$net) { // Kill off Gnutella clients, such as BearShare
@@ -83,7 +41,7 @@
 		}
 	}
 	else {
-		echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>Cachechu!</title></head><body><p>I Choose You! Cachechu!</p></body></html>';
+		echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>Cachechu!</title></head><body><p>I Choose You! Cachechu!</p><form method="GET" action="."><div><label for="url">URL:</label> <input type="text" name="url" id="url" size="50"><input type="hidden" name="update" value="1"><input type="hidden" name="net" value="gnutella2"> <input type="submit"></div></form></body></html>';
 	}
 
 	if($ping) {
@@ -93,13 +51,13 @@
 	// Add host to cache
 	if($update && $host) {
 		$error = TRUE;
-		if(strpos($host, $_SERVER['REMOTE_ADDR']) === TRUE && preg_match(IP_REGEX, $host)) {
+		if(strpos($host, $_SERVER['REMOTE_ADDR']) !== FALSE && preg_match(IP_REGEX, $host)) {
 			list($ip, $port) = explode(':', $host);
 			$socket = @fsockopen($ip, $port, $error_num, $error, 5);
 			if($socket) {
-				if(@fwrite($socket, "GNUTELLA CONNECT/0.6\r\n\r\n") === TRUE) {
+				if(@fwrite($socket, "GNUTELLA CONNECT/0.6\r\n\r\n") !== FALSE) {
 					stream_set_timeout($socket, 5);
-					if(stream_get_contents($socket, 12 == 'GNUTELLA CONNECT/0.6')) {
+					if(stream_get_contents($socket, 12) === 'GNUTELLA CONNECT/0.6') {
 						$error = FALSE;
 					}
 				}
@@ -122,11 +80,13 @@
 				}
 			}
 
-			// Limit number of hosts in file
+			// Limit number of hosts in file, keeps hosts at the end of the list
 			$host_count = count($new_lines);
 			if($host_count > HOST_LIMIT) {
 				$new_lines = array_slice($new_lines, $host_count - HOST_LIMIT, HOST_LIMIT);
 			}
+			
+			// Save hosts file and ignores concurrency issues
 			@file_put_contents(HOST_PATH, implode("\r\n", $new_lines), LOCK_EX);
 			echo "I|update|OK\n";
 		} else {
@@ -142,46 +102,89 @@
 		$test_urls = array();
 		$urls = array();
 		$lines = file_exists(URL_PATH) ? file(URL_PATH) : array();
+		shuffle($lines); // Used to randomize URL testing
 		foreach($lines as $line) {
-			list($xurl, $time, $status) = explode('|', trim($line));
+			list($xurl, $time, $status, $ip) = explode('|', trim($line));
 			if($xurl) {
-				$urls[$xurl] = array('time' => $time, 'status' => $status);
-				if($time == 0 || time() - $time >= MAX_URL_AGE) {
-					$test_urls[$xurl] = TRUE; // Test old URLs
+				$urls[$xurl] = array('time' => $time, 'status' => $status, 'ip' => $ip);
+				if($time === 0 || time() - $time >= MAX_URL_AGE) {
+					$test_urls[$xurl] = $status; // Test old URLs
 				}
 			}
 		}
 
+		// Prioritize known good caches (Sorts to OK --> NEW --> BAD)
+		arsort($test_urls);
+
 		// Add submitted GWebCache, do not allow Coral Cache URLs and only add new URL
 		if(strpos($url, 'nyud.net') === FALSE && preg_match(URL_REGEX, $url) && !isset($urls[$url])) {
-			$urls[$url] = array('time' => 0, 'status' => 'UNKNOWN');
-			$test_urls[$url] = TRUE;
+			$urls[$url] = array('time' => 0, 'status' => 'NEW', 'ip' => '');
+			$test_urls[$url] = 'NEW';
 		}
 
-		// Test random GWebCache, possibility that the submitted cache will be tested
-		$rand_url = array_rand($test_urls);
-		if($rand_url) {
-			$status = test_url($rand_url);
-			if(!$status && !$urls[$rand_url]['status']) {
-				$urls[$rand_url] = NULL; // Remove from cache after testing FAILED a 2nd time
-			} else {
-				$urls[$rand_url]['time'] = time();
-				$urls[$rand_url]['status'] = $status ? 'OK' : 'FAILED';
+		// Test "random" GWebCache, submitted cache goes to the end of the list
+		$test_url = key($test_urls); // First URL
+		if(preg_match(URL_REGEX, $test_url, $matches)) {
+			$error = NULL;
+			$domain = $matches['domain'];
+			$ip = gethostbyname($domain);
+			$socket = NULL;
+			if($ip != $domain) { // If gethostbyname fails, it will return the tested domain
+				$error = TRUE;
+				$port = isset($matches['port']) && $matches['port'] ? $matches['port'] : 80;
+				ini_set('user_agent', 'Cachechu');
+				$socket = @fsockopen($domain, $port, $errno, $errstr, 5);
 			}
+			if($socket) {
+				$file = isset($matches['file']) ? $matches['file'] : '/'; // No need to URL encode
+				$out = "GET $file?get=1&net=gnutella2&client=TEST&version=Cachechu HTTP/1.0\r\n";
+				$out .= "Host: $domain\r\n";
+				$out .= "Connection: Close\r\n\r\n";
+				$response = '';
+				if(@fwrite($socket, $out) !== FALSE) {
+					stream_set_timeout($socket, 5);
+					$response = stream_get_contents($socket);
+				}
+				fclose($socket);
+				$pos = strpos($response, "\r\n\r\n");
+				if($pos !== FALSE) {
+					$contents = trim(substr($response, $pos + 4));
+					if($contents) {
+						$error = FALSE;
+					}
+					$lines = explode("\n", $contents);
+					foreach($lines as $line) {
+						if(!preg_match(OUTPUT_REGEX, $line)) {
+							$error = TRUE; // Contains invalid output
+							break;
+						}
+					}
+				}
+			}
+			if(is_null($error) || ($error && !$urls[$test_url]['status'])) {
+				$urls[$test_url] = NULL; // Remove from cache after testing BAD a 2nd time, or no IP
+			} else {
+				$urls[$test_url]['time'] = time();
+				$urls[$test_url]['status'] = $error ? 'BAD' : 'OK';
+				$urls[$test_url]['ip'] = $ip;
+			}
+		} else {
+			$urls[$test_url] = NULL; // For whatever reason the URL is invalid
 		}
 
 		// Generate URL output
 		$output = '';
 		foreach($urls as $xurl => $values) {
-			$output .= "$xurl|" . $values['time'] . '|' . $values['status'] . "|\r\n";
+			if($values) {
+				$output .= "$xurl|" . $values['time'] . '|' . $values['status'] . '|' . $values['ip'] . "|\r\n";
+			}
 		}
+
+		// Save URL file and ignore concurrency issues
 		@file_put_contents(URL_PATH, $output, LOCK_EX);
 		
-		if(isset($urls[$url]) && $urls[$url]['status'] != 'FAILED') {
-			echo "I|update|OK\n";
-		} else {
-			echo "I|update|WARNING|Rejected URL\n";
-		}
+		// Output update notice (returns OK on untested URLs)
+		echo isset($urls[$url]) && $urls[$url]['status'] != 'BAD' ? "I|update|OK\n" : "I|update|WARNING|Rejected URL\n";
 	}
 
 	if($get) {
@@ -206,7 +209,7 @@
 		foreach($lines as $line) {
 			list($url, $time, $status) = explode('|', $line);
 			$age = $time > 0 ? time() - $time : 3600;
-			if($age < MAX_URL_AGE && $status == 'OK') {
+			if($age < MAX_URL_AGE && $status === 'OK') {
 				echo "U|$url|$age\n";
 				++$count;
 			}
@@ -216,8 +219,8 @@
 		}
 	}
 
-	// Must output something
+	// Must output something if no requests cause output
 	if(!ob_get_contents()) {
-		echo "I|\n";
+		echo "I|something\n";
 	}
 ?>
