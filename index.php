71bf47d7c94bbd1943560fa415f7dd7a67146b19
Kevin O'Toole <kevin@otoole.dev>
2008-08-19 04:01:33 +0000

    Added config and readme files

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	//
 	// You should have received a copy of the GNU General Public License
 	// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	
+
 	function get_url($url) {
 		$url = urldecode(rtrim(preg_replace(INDEX_REGEX, '', $url), '/'));
 		if(strpos($url, 'nyud.net') === FALSE && preg_match(URL_REGEX, $url, $match)) {
@@ -24,17 +24,19 @@
 		}
 	}
 
-	define('HOST_PATH', 'data/hosts.dat');
-	define('URL_PATH', 'data/urls.dat');
-	define('BAN_PATH', 'data/bans.dat');
-	define('HOST_LIMIT', 50); // Store 50 hosts,
-	define('HOST_OUTPUT', 30); // Output at most 30 hosts
-	define('URL_OUTPUT', 15); // Output at most 15 URLs
-	define('MAX_HOST_AGE', 25200); // 7 hours
-	define('MAX_URL_AGE', 86400); // 24 hours
-	define('URL_DISPLAY_AGE', 604800); // 1 week
-	define('BAN_TIME', 3600); // 1 hour
-	define('ADVERTISE', FALSE);
+	define('CONFIG_PATH', 'config/config.ini');
+	$config = file_exists(CONFIG_PATH) ? @parse_ini_file(CONFIG_PATH, TRUE) : array();
+	$config['Host']['Age'] = isset($config['Host']['Age']) ? $config['Host']['Age'] : 25200;
+	$config['Host']['Output'] = isset($config['Host']['Output']) ? $config['Host']['Output'] : 30;
+	$config['Host']['Store'] = isset($config['Host']['Store']) ? $config['Host']['Store'] : 50;
+	$config['URL']['Age'] = isset($config['URL']['Age']) ? $config['URL']['Age'] : 604800;
+	$config['URL']['Output'] = isset($config['URL']['Output']) ? $config['URL']['Output'] : 15;
+	$config['URL']['TestAge'] = isset($config['URL']['TestAge']) ? $config['URL']['TestAge'] : 86400;
+	$config['Cache']['Advertise'] = isset($config['Cache']['Advertise']) ? $config['Cache']['Advertise'] : 0;
+	$config['Cache']['BanTime'] = isset($config['Cache']['BanTime']) ? $config['Cache']['BanTime'] : 3600;
+	$config['Path']['Ban'] = isset($config['Cache']['Ban']) ? $config['Cache']['Ban'] : 'data/bans.dat';
+	$config['Path']['Host'] = isset($config['Path']['Host']) ? $config['Path']['Host'] : 'data/hosts.dat';
+	$config['Path']['URL'] = isset($config['Cache']['URL']) ? $config['Cache']['URL'] : 'data/urls.dat';
 
 	$remote_ip = $_SERVER['REMOTE_ADDR'];
 	$now       = time();
@@ -60,24 +62,24 @@
 
 	// Basic spam protection (1 update per hour [default])
 	if($update) {
-		$bans = file_exists(BAN_PATH) ? @unserialize(file_get_contents(BAN_PATH)) : array();
+		$bans = file_exists($config['Path']['Ban']) ? @unserialize(file_get_contents($config['Path']['Ban'])) : array();
 		if($bans == FALSE) { $bans = array(); } // File could not be unserialized
-		if(isset($bans[$remote_ip]) && $now - $bans[$remote_ip] <= BAN_TIME) {
+		if(isset($bans[$remote_ip]) && $now - $bans[$remote_ip] <= $config['Cache']['BanTime']) {
 			die("ERROR: Client returned too early\n");
 		} else {
 			foreach($bans as $ip => $time) {
-				if($now - $time > BAN_TIME) {
+				if($now - $time > $config['Cache']['BanTime']) {
 					unset($bans[$ip]); // Remove old banned hosts
 				}
 			}
 			$bans[$remote_ip] = $now; // Add current IP to banned list
-			@file_put_contents(BAN_PATH, serialize($bans));
-			echo "I|update|period|", BAN_TIME, "\n";
+			@file_put_contents($config['Path']['Ban'], serialize($bans));
+			echo "I|update|period|", $config['Cache']['BanTime'], "\n";
 		}
 	}
 
 	// Pong!
-	if($ping) { echo "I|pong|Cachechu R14|gnutella2\n"; }
+	if($ping) { echo "I|pong|Cachechu R16|gnutella2\n"; }
 
 	// Add host to cache
 	if($update && $host) {
@@ -99,13 +101,13 @@
 		if(!$error) {
 			// Add new host to hosts file and do a little cleanup of the file
 			$new_lines = array();
-			$lines = file_exists(HOST_PATH) ? file(HOST_PATH) : array();
+			$lines = file_exists($config['Path']['Host']) ? file($config['Path']['Host']) : array();
 			$client = preg_replace('/\\r\\n\\|/', '', $_SERVER['HTTP_USER_AGENT']); //  Don't want no problems in host file
 			$lines[] = "$host|" . $now . "|$client|\n";
 			foreach($lines as $line) {
 				list($ip, $time, $client) = explode('|', $line);
 				$age = $now - $time;
-				if($age < MAX_HOST_AGE && preg_match(IP_REGEX, $ip)) {
+				if($age < $config['Host']['Age'] && preg_match(IP_REGEX, $ip)) {
 					list($ip, $port) = explode(':', $ip);
 					// Remove duplicates and old hosts
 					$new_lines[$ip] = "$ip:$port|$time|$client|";
@@ -114,12 +116,12 @@
 
 			// Limit number of hosts in file, keeps hosts at the end of the list
 			$host_count = count($new_lines);
-			if($host_count > HOST_LIMIT) {
-				$new_lines = array_slice($new_lines, $host_count - HOST_LIMIT, HOST_LIMIT);
+			if($host_count > $config['Host']['Store']) {
+				$new_lines = array_slice($new_lines, $host_count - $config['Host']['Store'], $config['Host']['Store']);
 			}
 
 			// Save hosts file and ignore concurrency issues
-			@file_put_contents(HOST_PATH, implode("\r\n", $new_lines), LOCK_EX);
+			@file_put_contents($config['Path']['Host'], implode("\r\n", $new_lines), LOCK_EX);
 			echo "I|update|OK\n";
 		} else {
 			echo "I|update|WARNING|Rejected IP\n";
@@ -132,14 +134,14 @@
 		define('INDEX_REGEX', '/(?:default|index)\\.(?:aspx?|cfm|cgi|htm|html|jsp|php)$/iD');
 		$test_urls = array();
 		$urls = array();
-		$lines = file_exists(URL_PATH) ? file(URL_PATH) : array();
+		$lines = file_exists($config['Path']['URL']) ? file($config['Path']['URL']) : array();
 		shuffle($lines); // Used to randomize URL testing
 		foreach($lines as $line) {
 			list($xurl, $time, $status, $ip) = explode('|', trim($line));
 			$xurl = get_url($xurl);
 			if($xurl !== FALSE) {
 				$urls[$xurl] = array('time' => $time, 'status' => $status, 'ip' => $ip);
-				if($time === 0 || $now - $time >= MAX_URL_AGE) {
+				if($time === 0 || $now - $time >= $config['URL']['TestAge']) {
 					$test_urls[$xurl] = $status; // Test old URLs
 				}
 			}
@@ -173,7 +175,7 @@
 			if($socket) {			
 				$file = isset($match['file']) ? $match['file'] : '/'; // No need to URL encode
 				$query = "$file?get=1&net=gnutella2&client=TEST&version=Cachechu";
-				if(ADVERTISE) {
+				if($config['Cache']['Advertise']) {
 					$current_url = 'http://' . $_SERVER['SERVER_NAME'];
 					if($_SERVER['SERVER_PORT'] != 80) { $current_url .= ':' . $_SERVER['SERVER_PORT']; }
 					$current_url .= rtrim(str_replace('index.php', '', $_SERVER['PHP_SELF']), '/');
@@ -232,7 +234,7 @@
 		}
 		
 		// Save URL files and ignore concurrency issues
-		@file_put_contents(URL_PATH, $output, LOCK_EX);
+		@file_put_contents($config['Path']['URL'], $output, LOCK_EX);
 		// Output update notice (returns OK on untested URLs)
 		echo isset($urls[$url]) && $urls[$url]['status'] !== 'BAD' ? "I|update|OK\n" : "I|update|WARNING|Rejected URL\n";
 	}
@@ -240,34 +242,34 @@
 	if($get) {
 		// Output Hosts
 		$count = 0;
-		$lines = file_exists(HOST_PATH) ? file(HOST_PATH) : array();
+		$lines = file_exists($config['Path']['Host']) ? file($config['Path']['Host']) : array();
 		shuffle($lines);
 		foreach($lines as $line) {
 			list($ip, $time) = explode('|', $line);
 			$age = $now - $time;
-			if($age < MAX_HOST_AGE) {
+			if($age < $config['Host']['Age']) {
 				echo "H|$ip|$age\n";
 				++$count;
-				if($count >= HOST_OUTPUT) { break; }
+				if($count >= $config['Host']['Output']) { break; }
 			}
 		}
 		if(!$count) { echo "I|NO-HOSTS\n"; }
 
 		// Output URLs
 		$count = 0;
-		$lines = file_exists(URL_PATH) ? file(URL_PATH) : array();
+		$lines = file_exists($config['Path']['URL']) ? file($config['Path']['URL']) : array();
 		shuffle($lines);
 		foreach($lines as $line) {
 			list($url, $time, $status) = explode('|', $line);
 			$age = $time > 0 ? $now - $time : 3600;
-			if($age < URL_DISPLAY_AGE && $status === 'OK') {
+			if($age < $config['URL']['Age'] && $status === 'OK') {
 				echo "U|$url|$age\n";
 				++$count;
-				if($count >= URL_OUTPUT) { break; }
+				if($count >= $config['URL']['Output']) { break; }
 			}
 		}
 		if(!$count) { echo "I|NO-URLS\n"; }
 	}
 	
-	if(!empty($_GET)) { echo "I|access|period|1200\n"; }
+	if(!empty($_GET)) { echo "I|access|period|1800\n"; }
 ?>
