e29ca3fda151fcb6f040d6aac91440266efc8924
Kevin O'Toole <kevin.j.otoole@gmail.com>
2016-01-09 15:07:16 -0600

    Update version to 1.6.

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 	}
 	$now = time();
 	ob_start(); // Enable output buffering
-	define('VERSION', '1.5');
+	define('VERSION', '1.6');
 	define('AGENT', 'Cachechu ' . VERSION);
 	define('DEFAULT_NET', 'gnutella2');
 	define('MUTE', 'mute');
